d8a21714e9027111ce8138156e3e184b41d78da3
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2017-03-31 17:34:57 +0200

    Implement pluralize / singularize

--- modules/@metagram/framework/services/StringService.spec.ts
+++ modules/@metagram/framework/services/StringService.spec.ts
@@ -24,4 +24,28 @@ describe('StringService', () => {
     expect(StringService.camelToHyphenCase('HELLO')).to.equal('h-e-l-l-o');
     expect(StringService.camelToHyphenCase('hELLO')).to.equal('h-e-l-l-o');
   });
+
+  it('should pluralize words correctly', () => {
+    expect(StringService.pluralize('house')).to.equal('houses');
+    expect(StringService.pluralize('House')).to.equal('Houses');
+    expect(StringService.pluralize('mouse')).to.equal('mice');
+    expect(StringService.pluralize('reindeer')).to.equal('reindeer');
+    expect(StringService.pluralize('information')).to.equal('information');
+    expect(StringService.pluralize('datum')).to.equal('data');
+    expect(StringService.pluralize('child')).to.equal('children');
+    expect(StringService.pluralize('programme')).to.equal('programmes');
+    expect(StringService.pluralize('athletics')).to.equal('athletics');
+    expect(StringService.pluralize('thesis')).to.equal('theses');
+  });
+
+  it('should singularize words correctly', () => {
+    expect(StringService.singularize('houses')).to.equal('house');
+    expect(StringService.singularize('Houses')).to.equal('House');
+    expect(StringService.singularize('mice')).to.equal('mouse');
+    expect(StringService.singularize('reindeer')).to.equal('reindeer');
+    expect(StringService.singularize('information')).to.equal('information');
+    expect(StringService.singularize('data')).to.equal('datum');
+    expect(StringService.singularize('children')).to.equal('child');
+    expect(StringService.singularize('theses')).to.equal('thesis');
+  });
 });
